ce54b208eaef6fe5b1b3a79b97337aed50eb0541
송호준 <99067128+hojunking96@users.noreply.github.com>
2022-07-23 10:38:11 +0900

    [Silver II] Title: 랜선 자르기, Time: 212 ms, Memory: 18288 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        int K, N;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        int[] X = new int[K];
+        long max = 0, min = 1;
+
+        for (int i = 0; i < K; i++) {
+            X[i] = Integer.parseInt(br.readLine());
+            if (max < X[i])
+                max = X[i];
+        }
+        max++;
+        if (N == 1 && K == 1)
+            bw.write(X[0] + "\n");
+        else if (max == 2)
+            bw.write(1 + "\n");
+        else {
+            while (min < max) {
+                long mid = (max - min) / 2 + min;
+                int count = 0;
+                for (int i = 0; i < K; i++) {
+                    count += X[i] / mid;
+                }
+                if (count < N)
+                    max = mid;
+                else
+                    min = mid + 1;
+            }
+            bw.write(min - 1 + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
