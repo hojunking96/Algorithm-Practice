124a1df94084a415b7c7629dc8c53803b0f0447d
송호준 <99067128+hojunking96@users.noreply.github.com>
2022-07-28 21:17:05 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 552 ms, Memory: 116400 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -23,7 +23,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws IOException, NumberFormatException {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
