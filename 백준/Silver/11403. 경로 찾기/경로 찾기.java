e948e173ecf85d69334c345a64ddcaa8ba6ad5a7
송호준 <99067128+hojunking96@users.noreply.github.com>
2022-08-24 18:03:34 +0900

    [Silver I] Title: 경로 찾기, Time: 228 ms, Memory: 17180 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int N;
+    public static int[][] G;
+
+    public static void find(int[][] graph) {
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (graph[i][j] == 1)
+                        continue;
+                    if (graph[i][k] == 1 && graph[k][j] == 1)
+                        graph[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        G = new int[N][N];
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++)
+                G[i][j] = Integer.parseInt(st.nextToken());
+        }
+        find(G);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                bw.write(G[i][j] + " ");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
