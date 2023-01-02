8e3d616a537f068adfbb38cb0ba736f120843372
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-02 21:45:28 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -17,13 +17,16 @@ public class Main {
     }
 
     public static int GCD(int num1, int num2) {
-        while (num1 != num2) {
+        while (num1 * num2 != 0) {
             if (num1 > num2)
                 num1 -= num2;
             else
                 num2 -= num1;
         }
-        return num1;
+        if (num1 == 0)
+            return num2;
+        else
+            return num1;
     }
 
     public static int LST(int num1, int num2, int gcd) {
