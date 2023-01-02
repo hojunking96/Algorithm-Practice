40313c93c17a62d19edbc4b71f5cc3b3a2552793
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-02 16:26:18 +0900

    [Gold IV] Title: 검문, Time: 2844 ms, Memory: 14452 KB -BaekjoonHub

---
+++ 백준/Gold/2981. 검문/검문.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int N = Integer.parseInt(br.readLine());
+        int[] keys = new int[N];
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            keys[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(keys);
+        for (int i = 0; i < N - 1; i++) {
+            if (min >= keys[i + 1] - keys[i])
+                min = keys[i + 1] - keys[i];
+        }
+
+        for (int M = 2; M <= min; M++) {
+            int i;
+            int last = keys[0] % M;
+            for (i = 1; i < N; i++) {
+                if (keys[i] % M != last)
+                    break;
+            }
+            if (i == N)
+                pq.add(M);
+        }
+        while (!pq.isEmpty()) {
+            System.out.print(pq.poll() + " ");
+        }
+        br.close();
+    }
+}
