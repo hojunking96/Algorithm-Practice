e441171fb8255d53cfb2d88ecaed8350323a9cb4
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-07 20:09:16 +0900

    [Gold IV] Title: 검문, Time: 1436 ms, Memory: 17016 KB -BaekjoonHub

--- 백준/Gold/2981. 검문/검문.java
+++ 백준/Gold/2981. 검문/검문.java
@@ -1,38 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
         int N = Integer.parseInt(br.readLine());
-        int[] keys = new int[N];
-
-        int min = Integer.MAX_VALUE;
+        int[] nums = new int[N];
         for (int i = 0; i < N; i++) {
-            keys[i] = Integer.parseInt(br.readLine());
+            nums[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(keys);
-        for (int i = 0; i < N - 1; i++) {
-            min = Math.min(min, keys[i + 1] - keys[i]);
+        int[] diff = new int[N - 1];
+        for (int i = 0; i < N - 1; i++)
+            diff[i] = Math.abs(nums[i] - nums[i + 1]);
+        int gcd = diff[0];
+        for (int i = 1; i < N - 1; i++)
+            gcd = findGCD(gcd, diff[i]);
+        for (int i = 2; i <= gcd; i++) {
+            if (gcd % i == 0)
+                System.out.print(i + " ");
         }
+    }
 
-        for (int M = 2; M <= min; M++) {
-            int i;
-            int last = keys[0] % M;
-            for (i = 1; i < N; i++) {
-                if (keys[i] % M != last)
-                    break;
-            }
-            if (i == N)
-                pq.add(M);
+    private static int findGCD(int num1, int num2) {
+        int tmp, n;
+        if (num1 < num2) {
+            tmp = num1;
+            num1 = num2;
+            num2 = tmp;
         }
-        while (!pq.isEmpty()) {
-            System.out.print(pq.poll() + " ");
+        while (num2 != 0) {
+            n = num1 % num2;
+            num1 = num2;
+            num2 = n;
         }
-        br.close();
+        return num1;
     }
 }
