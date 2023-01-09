24aa5810ce1920e7876c053c1e4dc12860bb615d
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-09 15:36:19 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.12 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,65 +1,26 @@
-import java.math.BigInteger;
-import java.util.Stack;
-
 class Solution {
-    
     public int solution(int n, int k) {
-        int answer = 0;
-        answer = findPrime(convert(n, k));
-        return answer;
+        String[] converted = Integer.toString(n, k).split("0");
+        return findPrime(converted);
     }
-
-    public static int findPrime(String converted) {
-        int i;
+    
+    public static int findPrime(String[] converted) {
         int cnt = 0;
-        int startIndex = 0;
-        int endIndex = 0;
-        for (i = 0; i < converted.length(); i++) {
-            if (converted.charAt(i) == '0') {
-                if (converted.charAt(i - 1) == '0')
-                    continue;
-                endIndex = i;
-                String tmp = converted.substring(startIndex, endIndex);
-                if (primeCheck(tmp))
-                    cnt++;
-                startIndex = i + 1;
-            }
-        }
-        endIndex = i;
-        String tmp = converted.substring(startIndex, endIndex);
-        if (primeCheck(tmp))
-            cnt++;
+        for (int i = 0; i < converted.length; i++)
+            if (primeCheck(converted[i]))
+                cnt++;
         return cnt;
     }
 
     public static boolean primeCheck(String sub) {
+        if (sub.equals(""))
+            return false;
         Long checkNum = Long.parseLong(sub);
         if (checkNum < 2)
             return false;
-        int primeCnt = 0;
-        for (long i = 2; i <= Math.sqrt(checkNum) + 1; i++) {
-            if (i != checkNum && checkNum % i == 0) {
-                primeCnt++;
-            }
-            if (primeCnt > 0)
-                break;
-        }
-        if (primeCnt <= 0) {
-            return true;
-        }
-        return false;
-    }
-
-    public static String convert(int n, int k) {
-        Stack<Integer> stack = new Stack<>();
-        String converted = "";
-        int std = 0;
-        while (n != 0) {
-            stack.push(n % k);
-            n = n / k;
-        }
-        while (!stack.isEmpty())
-            converted += stack.pop();
-        return converted;
+        for (long i = 2; i <= Math.sqrt(checkNum); i++)
+            if (i != checkNum && checkNum % i == 0)
+                return false;
+        return true;
     }
 }
