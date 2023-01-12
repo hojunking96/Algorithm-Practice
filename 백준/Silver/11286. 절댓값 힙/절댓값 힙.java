13ac875e5d9b14cda2b0fff0a999f763ac7b5212
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-12 23:14:20 +0900

    [Silver I] Title: 절댓값 힙, Time: 352 ms, Memory: 26444 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            if (Math.abs(o1) > Math.abs(o2)) {
+                return Math.abs(o1) - Math.abs(o2);
+            } else if (Math.abs(o1) == Math.abs(o2)) {
+                return o1 - o2;
+            } else {
+                return -1;
+            }
+        });
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pq.isEmpty())
+                    sb.append("0\n");
+                else {
+                    sb.append(pq.poll() + "\n");
+                }
+            } else {
+                pq.add(x);
+            }
+        }
+        System.out.println(sb);
+    }
+}
