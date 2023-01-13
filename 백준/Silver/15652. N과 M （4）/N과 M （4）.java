1f0f20ce3ef9b63ea0a2373fd872f7aeed3245c3
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-14 03:35:24 +0900

    [Silver III] Title: N과 M (4), Time: 204 ms, Memory: 18872 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static int N;
+    public static int M;
+    public static StringBuilder sb;
+    public static int[] cnt;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        cnt = new int[N];
+        printSequence(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void printSequence(int depth, int start) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(cnt[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i < N; i++) {
+            cnt[depth] = i + 1;
+            printSequence(depth + 1, i);
+        }
+    }
+}
