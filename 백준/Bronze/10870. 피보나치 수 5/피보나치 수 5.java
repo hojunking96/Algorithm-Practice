2ddefb93c97a226c5c67d60fa8ba76af1c6f65d7
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-16 19:59:41 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 124 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,25 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
-        list.add(0);
-        list.add(1);
-        for (int i = 2; i <= N; i++) {
-            pibo(list, i);
-        }
-        System.out.println(list.get(N));
+        System.out.println(pibo(N));
     }
 
-    public static void pibo(List<Integer> list, int N) {
-        list.add(list.get(N - 1) + list.get(N - 2));
+    public static int pibo(int N) {
+        if (N == 0)
+            return 0;
+        if (N == 1)
+            return 1;
+        return (pibo(N - 1) + pibo(N - 2));
     }
 
 }
