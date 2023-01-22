40b647a2eee7fd84e5bf8e29669d830c18e87c41
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-22 16:04:32 +0900

    [Gold IV] Title: 플로이드, Time: 444 ms, Memory: 42364 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,58 +1,66 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {
-    public static int n, m, INF = Integer.MAX_VALUE;
-    public static int[][] G;
 
-    public static void init(int[][] graph) {
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                G[i][j] = INF;
+public class Main {
+    static final int INF = Integer.MAX_VALUE;
+    private static int[][] costs;
+    private static int N;
+    private static int M;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        costs = new int[N][N];
+        initCost();
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int dest = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            costs[start - 1][dest - 1] = Math.min(costs[start - 1][dest - 1], cost);
+        }
+        floydWarshall();
+        writeCosts();
+        System.out.println(sb);
     }
 
-    public static void FW(int[][] graph) {
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (i != j && graph[i][k] != INF && graph[k][j] != INF)
-                        graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+    public static void initCost() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == j) {
+                    costs[i][j] = 0;
+                } else {
+                    costs[i][j] = INF;
                 }
             }
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        n = Integer.parseInt(br.readLine());
-        G = new int[n][n];
-        init(G);
-        m = Integer.parseInt(br.readLine());
-
-        StringTokenizer st;
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            G[a - 1][b - 1] = Math.min(G[a - 1][b - 1], c);
+    public static void floydWarshall() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    if (costs[j][i] == INF || costs[i][k] == INF)
+                        continue;
+                    costs[j][k] = Math.min(costs[j][k], costs[j][i] + costs[i][k]);
+                }
+            }
         }
-        FW(G);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (G[i][j] == INF)
-                    G[i][j] = 0;
-                if (i == j)
-                    G[i][j] = 0;
-                sb.append(G[i][j] + " ");
+    }
+
+    public static void writeCosts() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (costs[i][j] == INF)
+                    sb.append(0).append(" ");
+                else {
+                    sb.append(costs[i][j]).append(" ");
+                }
             }
             sb.append("\n");
         }
-        System.out.println(sb);
-        br.close();
     }
 }
