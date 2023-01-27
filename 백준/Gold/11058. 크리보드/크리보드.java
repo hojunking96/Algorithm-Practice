c6ff0bb90e1ca9da8da05f541d408f7d326f6001
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-27 22:44:22 +0900

    [Gold V] Title: 크리보드, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Gold/11058. 크리보드/크리보드.java
+++ 백준/Gold/11058. 크리보드/크리보드.java
@@ -6,12 +6,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         long[] dp = new long[N + 1];
-        dp[1] = 1;
         for (int i = 1; i <= N; i++) {
-            dp[i] = dp[i - 1] + 1;
-            if (i > 6) {
-                for (int j = 2; j < i; j++) {
-                    dp[i] = Math.max(dp[i], dp[i - j - 1] * j);
+            dp[i] = i;
+            if (i >= 4) {
+                for (int j = 3; j <= i; j++) {
+                    dp[i] = Math.max(dp[i], dp[i - j] * (j - 1));
                 }
             }
         }
