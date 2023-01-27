e25caefa61709c444e09acc79e5c1147e02c94ae
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-28 06:53:01 +0900

    [Gold V] Title: 평범한 배낭, Time: 156 ms, Memory: 14708 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,43 +1,27 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-        int N, K;
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
         int[] W = new int[N + 1];
         int[] V = new int[N + 1];
-        int[][] dp = new int[N + 1][K + 1];
-
+        int[] dp = new int[K + 1]; //index 까지의 최대 무게
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             W[i] = Integer.parseInt(st.nextToken());
             V[i] = Integer.parseInt(st.nextToken());
         }
         for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= K; j++) {
-                dp[i][j] = dp[i - 1][j]; // 기본적으로 이전 아이템의 가치를 저장한다.
-                if (j - W[i] >= 0) { // 무게에서 자신의 무게를 뺐을 때 남는 무게가 존재하면,
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]); // 이전 아이템에서 구한 가치와 남은 무게의 가치 + 자신의 가치
-                                                                                   // 중 큰 값을 취한다.
-                }
+            for (int j = K; j - W[i] >= 0; j--) {
+                dp[j] = Math.max(dp[j], dp[j - W[i]] + V[i]);
             }
         }
-        bw.write(dp[N][K] + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(dp[K]);
+
     }
 }
