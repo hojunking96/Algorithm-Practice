139662bd8d0016daa01278ddc1676a0694c3172d
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-29 18:36:35 +0900

    [Gold V] Title: 합분해, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[K + 1][N + 1];       //첫 번째 인덱스 : 개수, 두 번째 인덱스 : 총 합
+
+        for (int i = 0; i <= N; i++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 1; i <= K; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 2; i <= K; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000000;
+            }
+        }
+        System.out.println(dp[K][N]);
+    }
+}
