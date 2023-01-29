2e309028009afd554dfbc4ee8a6ea8d54234729c
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-29 20:19:27 +0900

    [Gold V] Title: Contact, Time: 208 ms, Memory: 16588 KB -BaekjoonHub

---
+++ 백준/Gold/1013. Contact/Contact.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        String pattern = "(100+1+|01)+";
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            if (input.matches(pattern)) {
+                sb.append("YES\n");
+            } else {
+                sb.append("NO\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
