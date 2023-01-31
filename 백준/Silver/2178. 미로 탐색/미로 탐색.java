cabc5e9e75391a3acac706f742d8a057992c3a4c
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-02-01 03:29:54 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 15460 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -11,27 +11,44 @@ public class Main {
     static int[][] X;
 
     static int N, M;
-    static int[] dx = { 1, 0, -1, 0 };
-    static int[] dy = { 0, 1, 0, -1 };
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
-    static class pair {
+    static class XY {
         int x;
         int y;
 
-        pair(int x, int y) {
+        XY(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
 
-    static void BFS(int x, int y) {
-        Queue<pair> q = new LinkedList<>();
-        q.add(new pair(x, y));
-        while (!q.isEmpty()) {
-            pair now = q.poll();
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = new int[N][M];
 
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                X[i][j] = s.charAt(j) - '0';
+            }
+        }
+        BFS(0, 0);
+        System.out.println(X[N - 1][M - 1]);
+        br.close();
+    }
+
+    public static void BFS(int x, int y) {
+        Queue<XY> q = new LinkedList<>();
+        q.add(new XY(x, y));
+        while (!q.isEmpty()) {
+            XY now = q.poll();
             for (int i = 0; i < 4; i++) {
-                pair next = new pair(now.x + dx[i], now.y + dy[i]);
+                XY next = new XY(now.x + dx[i], now.y + dy[i]);
                 if (next.x >= 0 && next.x <= N - 1 && next.y >= 0 && next.y <= M - 1) {
                     if (X[next.x][next.y] == 1) {
                         X[next.x][next.y] = X[now.x][now.y] + 1;
@@ -42,25 +59,4 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws IOException, NumberFormatException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        X = new int[N][M];
-
-        for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < M; j++)
-                X[i][j] = s.charAt(j) - '0';
-        }
-        BFS(0, 0);
-        bw.write(X[N - 1][M - 1] + "\n");
-
-        bw.flush();
-        bw.close();
-        br.close();
-
-    }
 }
