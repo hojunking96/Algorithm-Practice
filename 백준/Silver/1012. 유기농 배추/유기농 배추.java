5b477db05c5b508119b5a2dae520dc827a76c7d4
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-31 14:27:34 +0900

    [Silver II] Title: 유기농 배추, Time: 160 ms, Memory: 15824 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,71 +1,55 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
-class pair {
-    int x;
-    int y;
-
-    pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int X[][];
-    static int K, N, M;
-    static int[] dx = { 1, 0, -1, 0 };
-    static int[] dy = { 0, 1, 0, -1 };
 
-    static void DFS(pair now) {
-        X[now.x][now.y] = 0;
-        for (int i = 0; i < 4; i++) {
-            pair next = new pair(now.x + dx[i], now.y + dy[i]);
-            if (next.x >= 0 && next.x < N && next.y >= 0 && next.y < M) {
-                if (X[next.x][next.y] != 0)
-                    DFS(next);
-            }
-        }
-    }
+    public static int N, T, M, K;
+    public static boolean[][] graph;
+    public static boolean[][] visited;
 
-    public static void main(String[] args) throws IOException, NumberFormatException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
-
-        for (int caseT = 0; caseT < T; caseT++) {
+        T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
             st = new StringTokenizer(br.readLine());
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
-            int cnt = 0;
-            X = new int[N][M];
-
+            graph = new boolean[N + 2][M + 2];
+            visited = new boolean[N + 2][M + 2];
             for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
-                int tmpX = Integer.parseInt(st.nextToken());
-                int tmpY = Integer.parseInt(st.nextToken());
-                X[tmpY][tmpX] = 1;
+                int Y = Integer.parseInt(st.nextToken());
+                int X = Integer.parseInt(st.nextToken());
+                graph[X + 1][Y + 1] = true;
             }
-
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (X[i][j] == 1) {
-                        DFS(new pair(i, j));
+            int cnt = 0;
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= M; j++) {
+                    if (graph[i][j] && !visited[i][j]) {
+                        DFS(i, j);
                         cnt++;
                     }
                 }
             }
-            bw.write(cnt + "\n");
-
+            System.out.println(cnt);
         }
-        bw.flush();
-        bw.close();
-        br.close();
+    }
+
+    public static void DFS(int startX, int startY) {
+        visited[startX][startY] = true;
+        if (graph[startX + 1][startY] && !visited[startX + 1][startY]) {
+            DFS(startX + 1, startY);
+        }
+        if (graph[startX][startY + 1] && !visited[startX][startY + 1]) {
+            DFS(startX, startY + 1);
+        }
+        if (graph[startX - 1][startY] && !visited[startX - 1][startY]) {
+            DFS(startX - 1, startY);
+        }
+        if (graph[startX][startY - 1] && !visited[startX][startY - 1]) {
+            DFS(startX, startY - 1);
+        }
     }
 }
