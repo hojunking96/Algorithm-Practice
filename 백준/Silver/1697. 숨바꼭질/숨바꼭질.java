c1a57b2788ea26322f565a0d433b1dbeb16f088d
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-01-31 21:49:37 +0900

    [Silver I] Title: 숨바꼭질, Time: 180 ms, Memory: 20240 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -8,12 +8,14 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static boolean check[] = new boolean[100001];
     static int X[] = new int[100001];
     static int N, K;
 
     static void BFS(int root) {
         Queue<Integer> q = new LinkedList<>();
         q.add(root);
+        check[root] = true;
         X[root] = 1;
         while (!q.isEmpty()) {
             int now = q.poll();
@@ -21,22 +23,25 @@ public class Main {
                 break;
             int n1 = now + 1;
             if (n1 >= 0 && n1 <= 100000) {
-                if (X[n1] == 0) {
+                if (!check[n1] && X[n1] == 0) {
+                    check[n1] = true;
                     X[n1] = X[now] + 1;
                     q.add(n1);
                 }
             }
             int n2 = now - 1;
             if (n2 >= 0 && n2 <= 100000) {
-                if (X[n2] == 0) {
+                if (!check[n2] && X[n2] == 0) {
                     X[n2] = X[now] + 1;
+                    check[n2] = true;
                     q.add(n2);
                 }
             }
             int n3 = now * 2;
             if (n3 >= 0 && n3 <= 100000) {
-                if (X[n3] == 0) {
+                if (!check[n3] && X[n3] == 0) {
                     X[n3] = X[now] + 1;
+                    check[n3] = true;
                     q.add(n3);
                 }
             }
