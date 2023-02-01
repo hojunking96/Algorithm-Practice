4c0279de0f335999c7d444185d477018f11790ee
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-02-01 20:16:35 +0900

    [Silver III] Title: N과 M (5), Time: 356 ms, Memory: 29696 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int N, M;
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] arr;
+    public static int[] newArr;
+    public static boolean[] visited;
+
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            pq.add(Integer.parseInt(st.nextToken()));
+        }
+        arr = new int[N];
+        newArr = new int[M];
+        visited = new boolean[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = pq.poll();
+        }
+        backTracking(0);
+        System.out.println(sb);
+    }
+    
+    public static void backTracking(int depth) {
+        if (depth == M) {
+            for (int e : newArr) {
+                sb.append(e).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                newArr[depth] = arr[i];
+                visited[i] = true;
+                backTracking(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
