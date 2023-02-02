faaab6563ec367293e2d93b1c63a0741d01c9d16
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-02-02 13:12:04 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 148 ms, Memory: 18540 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,94 +1,57 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    private static final int INF = Integer.MAX_VALUE;
-    static int[] T;
-    static int N, K;
-    static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
+    public static int N, K;
+    public static int[] graph;
+    public static boolean[] visited;
 
-    private static class Node implements Comparable<Node> {
-        private int index;
-        private int time;
-
-        public Node(int index, int time) {
-            this.index = index;
-            this.time = time;
-        }
-
-        public int getIndex() {
-            return this.index;
-        }
-
-        public int getTime() {
-            return this.time;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            if (this.time < o.time)
-                return -1;
-            return 1;
-        }
-    }
-
-    static void dijkstra(int start) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(start, 0));
-        T[N] = 0;
-        while (!pq.isEmpty()) {
-            Node node = pq.poll();
-            int now = node.getIndex();
-            int t = node.getTime();
-
-            now++;
-            t++;
-
-            if (now <= 100000 && t < T[now]) {
-                T[now] = t;
-                pq.add(new Node(now, t));
-            }
-
-            now -= 2;
-            if (0 <= now && t < T[now]) {
-                T[now] = t;
-                pq.add(new Node(now, t));
-            }
-            now = (now + 1) * 2;
-            t--;
-            if (now <= 100000 && t < T[now]) {
-                T[now] = t;
-                pq.add(new Node(now, t));
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NumberFormatException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
+        graph = new int[100001];
+        visited = new boolean[100001];
+        BFS();
+        System.out.println(graph[K]);
+    }
 
-        T = new int[100001];
-        Arrays.fill(T, INF);
-
-        for (int i = 1; i <= N; i++) {
-            graph.add(new ArrayList<>());
+    public static void BFS() {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(N);
+        visited[N] = true;
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            if (now == K) {
+                break;
+            }
+            int next = now * 2;
+            if (next <= 100000) {
+                if (!visited[next]) {
+                    q.add(next);
+                    graph[next] = graph[now];
+                    visited[next] = true;
+                }
+            }
+            next = now - 1;
+            if (next >= 0) {
+                if (!visited[next]) {
+                    q.add(next);
+                    graph[next] = graph[now] + 1;
+                    visited[next] = true;
+                }
+            }
+            next = now + 1;
+            if (next <= 100000) {
+                if (!visited[next]) {
+                    q.add(next);
+                    graph[next] = graph[now] + 1;
+                    visited[next] = true;
+                }
+            }
         }
-
-        dijkstra(N);
-        bw.write(T[K] + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
