64f986ab79a278338c05968c5a1bef4a45e23ac0
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-02-09 20:15:52 +0900

    [Bronze II] Title: 블랙잭, Time: 136 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(bruteForce(arr, N, M));
+    }
+
+    public static int bruteForce(int[] arr, int N, int M) {
+        int result = 0;
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int temp = arr[i] + arr[j] + arr[k];
+                    if (M == temp) {
+                        return temp;
+                    }
+                    if (result < temp && temp < M) {
+                        result = temp;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
