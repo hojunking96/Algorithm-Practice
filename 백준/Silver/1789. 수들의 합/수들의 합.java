c3709ec554a37bc1acb88d30c45eab612308e132
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-02-14 22:24:22 +0900

    [Silver V] Title: 수들의 합, Time: 128 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.java
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -1,25 +1,19 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        long N, S;
+    
+    public static void main(String[] args) throws IOException, NumberFormatException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        S = Long.parseLong(br.readLine());
-        for (N = 1; N <= S; N++) {
-            if (N * (N + 1) / 2 <= S && (N + 1) * (N + 2) / 2 > S) {
-                bw.write(N + "\n");
+        long S = Long.parseLong(br.readLine());
+        long sum = 0;
+        for (int i = 1; i <= Integer.MAX_VALUE; i++) {
+            sum += i;
+            if (sum > S) {
+                System.out.println(i - 1);
                 break;
             }
         }
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
