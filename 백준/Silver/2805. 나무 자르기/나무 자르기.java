b6d4eb288ec9d252f415e3da7c65c57936190e9d
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-02-14 23:21:06 +0900

    [Silver II] Title: 나무 자르기, Time: 548 ms, Memory: 123332 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,47 +1,41 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException, NumberFormatException {
-        int N, M;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        int[] X = new int[N];
-        int low = 0;
-        int high = 0;
-        long sum = 0;
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        long[] arr = new long[N];
+
+        long left = 0;
+        long right = 0;
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            X[i] = Integer.parseInt(st.nextToken());
-            if (X[i] > high)
-                high = X[i];
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (arr[i] > right) {
+                right = arr[i];
+            }
         }
-        Arrays.sort(X);
-        high++;
-
-        while (low < high) {
-            int mid = (high - low) / 2 + low;
-            sum = 0;
-            for (int i = 0; i < N; i++)
-                if (X[i] > mid)
-                    sum += X[i] - mid;
-            if (sum < M)
-                high = mid;
-            else
-                low = mid + 1;
+        right++;
+        while (left < right) {
+            long mid = (right - left) / 2 + left;
+            long sum = 0;
+            for (int j = 0; j < N; j++) {
+                if (arr[j] > mid) {
+                    sum += arr[j] - mid;
+                }
+            }
+            if (sum < M) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
-        bw.write(low - 1 + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(left - 1);
     }
 }
