556ba6a23c87220058dd28c592fa1e1baa162054
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-02-22 16:41:29 +0900

    [level 0] Title: 배열의 평균값, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,9 +1,9 @@
 class Solution {
     public double solution(int[] numbers) {
-        double sum=0;
-        for(int i=0;i<numbers.length;i++){
-            sum+=numbers[i];
+        double sum = 0;
+        for(int i = 0;i < numbers.length; i++){
+            sum += numbers[i];
         }
-        return sum/numbers.length;
+        return sum / numbers.length;
     }
 }
