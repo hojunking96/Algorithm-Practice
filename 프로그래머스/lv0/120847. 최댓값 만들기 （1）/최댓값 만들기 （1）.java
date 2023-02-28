a368b4b1fc579ce287f597ea33c37e63d7445b38
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-03-01 04:12:34 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.53 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,13 +1,7 @@
+import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        for(int i=0;i<numbers.length;i++){
-            for(int j=i+1;j<numbers.length;j++){
-                if(numbers[i]*numbers[j]>answer){
-                    answer=numbers[i]*numbers[j];
-                }
-            }
-        }
-        return answer;
+        Arrays.sort(numbers);
+        return numbers[numbers.length-1]*numbers[numbers.length-2];
     }
 }
