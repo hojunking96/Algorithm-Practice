14f7bd274332223383062bee2e79a236884121a1
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-03-07 09:37:57 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int hp) {
         int ant1 = hp / 5;
-        hp -= ant1 * 5;
+        hp %=5;
         int ant2 = hp / 3;
-        hp -= ant2 * 3;
+        hp %=3;
         int ant3 = hp;
         return ant1+ant2+ant3;
     }
