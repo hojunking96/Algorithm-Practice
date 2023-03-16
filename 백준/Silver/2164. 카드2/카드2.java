2cb3abd4f77b348977e4820cf525e88743be5519
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-03-16 17:38:18 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 53300 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,25 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer> q = new LinkedList<>();
         int N = Integer.parseInt(br.readLine());
-        Deque<Integer> cards = new LinkedList<>();
+        if (N == 1) {
+            System.out.println(1);
+            return;
+        }
+        int answer;
         for (int i = 1; i <= N; i++) {
-            cards.offerLast(i);
+            q.add(i);
         }
-        while (cards.size() != 1) {
-            cards.poll();
-            if (cards.size() == 1)
+        while (true) {
+            q.poll();
+            if (q.size() == 1) {
+                answer = q.poll();
                 break;
-            int topCard = cards.poll();
-            cards.offerLast(topCard);
+            }
+            int tmp = q.poll();
+            q.add(tmp);
         }
-        System.out.println(cards.peek());
-
+        System.out.println(answer);
     }
 }
