cfb6bd260b5ee991e59b00b67019bd873e560055
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-03-19 00:04:12 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -4,7 +4,7 @@ class Solution {
         int[] answer = new int[(n+1)/2];
         for(int i=1;i<=n;i+=2){
             if(i%2==1){
-              answer[idx]=i;  
+                answer[idx]=i;  
                 idx++;
             }
         }
