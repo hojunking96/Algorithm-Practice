3500d85aaa93651c18fb4c3d1b1df3600d8a5fbb
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-03-30 16:20:57 +0900

    [Silver IV] Title: 수 찾기, Time: 588 ms, Memory: 40652 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,56 +1,48 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean find(int X[], int low, int high, int k) {
-        int mid;
-        while (low <= high) {
-            mid = (low + high) / 2;
-            if (k == X[mid]) {
-                return true;
-            } else if (k > X[mid])
-                low = mid + 1;
-            else
-                high = mid - 1;
+    private static int N;
+    private static int[] arr;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        return false;
+        Arrays.sort(arr);
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            boolean isPresent = binarySearch(key, 0, N - 1);
+            if (isPresent) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
+            }
+        }
+        System.out.println(sb);
     }
 
-    public static void main(String[] args) throws IOException, NumberFormatException {
-        int N, M;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        N = Integer.parseInt(br.readLine());
-        int[] X = new int[N + 1];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            X[i] = Integer.parseInt(st.nextToken());
+    private static boolean binarySearch(int key, int startIdx, int endIdx) {
+        while (startIdx <= endIdx) {
+            int midIdx = (startIdx + endIdx) / 2;
+            if (arr[midIdx] == key) {
+                return true;
+            } else if (arr[midIdx] > key) {
+                endIdx = midIdx - 1;
+            } else {
+                startIdx = midIdx + 1;
+            }
         }
-
-        Arrays.sort(X, 1, N + 1);
-
-        M = Integer.parseInt(br.readLine());
-        int[] Y = new int[M];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++)
-            Y[i] = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < M; i++) {
-            if (find(X, 1, N, Y[i]))
-                bw.write(1 + "\n");
-            else
-                bw.write(0 + "\n");
-
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
+        return false;
     }
 }
