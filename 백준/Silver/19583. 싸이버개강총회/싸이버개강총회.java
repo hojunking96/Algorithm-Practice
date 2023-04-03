1585f2dea41d30ca840ccfcfa94c44287521ad20
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-04-04 01:57:40 +0900

    [Silver II] Title: 싸이버개강총회, Time: 488 ms, Memory: 46304 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
@@ -8,26 +8,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] times = br.readLine().split(" ");
-        Set<String>[] members = new Set[2];
-        for (int i = 0; i < 2; i++) {
-            members[i] = new HashSet<>();
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String s = st.nextToken();
+        String e = st.nextToken();
+        String q = st.nextToken();
+
+        Set<String> before = new HashSet<>();
+        Set<String> after = new HashSet<>();
 
         String str = null;
         while ((str = br.readLine()) != null) {
-            String[] str2 = str.split(" ");
-            String name = str2[1];
-            if (str2[0].compareTo(times[0]) <= 0) {
-                members[0].add(name);
-            } else if (str2[0].compareTo(times[1]) >= 0 && str2[0].compareTo(times[2]) <= 0) {
-                members[1].add(name);
+            st = new StringTokenizer(str);
+            String time = st.nextToken();
+            String name = st.nextToken();
+            if (time.compareTo(s) <= 0) {
+                before.add(name);
+            } else if (time.compareTo(e) >= 0 && time.compareTo(q) <= 0) {
+                after.add(name);
             }
         }
 
         int cnt = 0;
-        for (String name : members[0]) {
-            if (members[1].contains(name)) {
+        for (String name : before) {
+            if (after.contains(name)) {
                 cnt++;
             }
         }
