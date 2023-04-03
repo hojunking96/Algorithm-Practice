6fdec66a9bc258fe8c884afbd69c7fdbdad761bc
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-04-03 17:12:32 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,29 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		int N, K, cnt = 0;
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		K = sc.nextInt();
-		int []x = new int[N];
-		for (int i = 0; i < N; i++) {
-			x[i] = sc.nextInt();
-		}
-		for (int i = N - 1; i >= 0; i--) {
-			while (x[i] <= K) {
-				if (x[i] <= K) {
-					K -= x[i];
-					cnt++;
-				}
-			}
-			if (K == 0)
-				break;
-		}
-		System.out.println(cnt);
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        Integer[] arr = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            cnt += K / arr[i];
+            K %= arr[i];
+            if (K == 0) {
+                break;
+            }
+        }
+        System.out.println(cnt);
+    }
 }
-
-
