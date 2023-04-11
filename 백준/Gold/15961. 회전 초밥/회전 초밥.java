584a07178a94d8f00939303390937dd643b5a3b8
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-04-11 21:40:11 +0900

    [Gold IV] Title: 회전 초밥, Time: 524 ms, Memory: 171236 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -5,63 +5,71 @@ import java.util.*;
 
 public class Main {
 
-    private static int N, d, k, c;
-    private static int[] arr;
-    private static int max;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-        arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
+        int[] cnt = new int[d + 1];
+        int[] arr = new int[k];
+        int[] history = new int[N + k];
+        int numCnt = 0;
 
-        Map<Integer, Integer> map = new HashMap<>();
-
+        //k개 윈도우
         for (int i = 0; i < k; i++) {
-            int idx = i;
-            if (i > N) {
-                idx = i % N;
+            int next = Integer.parseInt(br.readLine());
+            if (cnt[next] == 0) {
+                numCnt++;
             }
-            if (map.containsKey(arr[idx])) {
-                int value = map.get(arr[idx]);
-                map.replace(arr[idx], value + 1);
+            cnt[next]++;
+            history[i] = next;
+            arr[i] = next;
+        }
+        int max = numCnt;
+
+        //i가 1씩 증가하면서 윈도우 1칸씩 이동
+        for (int i = k; i < N; i++) {
+            int next = Integer.parseInt(br.readLine());
+
+            if (cnt[next] == 0) {
+                numCnt++;
+            }
+            cnt[next]++;
+            history[i] = next;
+
+            if (cnt[history[i - k]] == 1) {
+                numCnt--;
+            }
+            cnt[history[i - k]]--;
+
+            if (cnt[c] == 0) {
+                max = Math.max(max, numCnt + 1);
             } else {
-                map.put(arr[idx], 1);
+                max = Math.max(max, numCnt);
             }
         }
-
-        max = map.size();
-        for (int i = 1; i < N; i++) {
-            int left = (i - 1 + N) % N;
-            int right = (i + k - 1) % N;
-
-            if (map.get(arr[left]) == 1) {
-                map.remove(arr[left]);
-            } else {
-                int value = map.get(arr[left]);
-                map.replace(arr[left], value - 1);
+        //n 까지 돌고 남은 것
+        for (int i = 0; i < k; i++) {
+            int next = arr[i];
+            if (cnt[next] == 0) {
+                numCnt++;
             }
+            cnt[next]++;
+            history[N + i] = next;
 
-            if (map.containsKey(arr[right])) {
-                int value = map.get(arr[right]);
-                map.replace(arr[right], value + 1);
-            } else {
-                map.put(arr[right], 1);
+            if (cnt[history[i - k + N]] == 1) {
+                numCnt--;
             }
-            if (!map.containsKey(c)) {
-                max = Math.max(max, map.size() + 1);
-                if (max == k + 1) {
-                    break;
-                }
+            cnt[history[i - k + N]]--;
+
+            if (cnt[c] == 0) {
+                max = Math.max(max, numCnt + 1);
             } else {
-                max = Math.max(max, map.size());
+                max = Math.max(max, numCnt);
             }
         }
         System.out.println(max);
