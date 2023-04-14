1548815834c799087d3e933c9ac18200cca1f2b0
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-04-14 13:40:21 +0900

    [Bronze V] Title: 문자열, Time: 152 ms, Memory: 15864 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            System.out.println(""+input.charAt(0) +input.charAt(input.length() - 1));
+        }
+
+    }
+}
