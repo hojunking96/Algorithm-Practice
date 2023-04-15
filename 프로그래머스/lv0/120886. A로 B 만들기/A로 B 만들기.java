9ca70420f70fcadee041307b4cf49494fb04a0c6
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-04-15 15:34:41 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String before, String after) {
+        int[] cnt = new int[26];
+        for(int i=0;i<before.length();i++){
+            cnt[before.charAt(i)-'a']++;
+        }
+        for(int i=0;i<after.length();i++){
+            cnt[after.charAt(i)-'a']--;
+        }
+        int answer = 1;
+        for(int i=0;i<26;i++){
+            if(cnt[i]!=0){
+                answer=0;
+                break;
+            }
+        }
+        return answer;
+    }
+}
