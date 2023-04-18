32e850ac67789583bc80ff67667d128be3be62a5
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-04-18 09:36:58 +0900

    [level 2] Title: 숫자의 표현, Time: 0.89 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,14 +1,13 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int num = 1;
-        int temp = 0;
+        
         for (int i = 1; i <= n / 2; i++){
-            temp = 0;
-            num = i;
-            while(temp <= n){
-                temp += num++;
-                if(temp == n){ 
+            int sum = 0;
+            int num = i;
+            while(sum <= n){
+                sum += num++;
+                if(sum == n){ 
                     answer++;
                 }
             }
