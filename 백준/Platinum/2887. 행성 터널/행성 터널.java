cfa9c95c16d1769311afe20c1fa87e6fafefedf4
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-04-18 14:06:09 +0900

    [Platinum V] Title: 행성 터널, Time: 1916 ms, Memory: 78636 KB -BaekjoonHub

---
+++ 백준/Platinum/2887. 행성 터널/행성 터널.java
@@ -0,0 +1,125 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private static class Planet {
+        int num;
+        int x;
+        int y;
+        int z;
+
+        public Planet(int num, int x, int y, int z) {
+            this.num = num;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    private static class Edge {
+        public int start;
+        public int end;
+        public long weight;
+
+        public Edge(int start, int end, long weight) {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+    }
+
+    private static int N;
+    private static int[] parent;
+    private static List<Edge> edges = new ArrayList<>();
+    private static List<Planet> planets = new ArrayList<>();
+
+
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+            planets.add(new Planet(i, A, B, C));
+        }
+// x, y, z 각각에 대해서 정렬하고 각 행성의 번호와 비용을 edgeList에 추가.
+        Collections.sort(planets, Comparator.comparingInt(o -> o.x));
+        for (int i = 0; i < N - 1; i++) {
+            int weight = Math.abs(planets.get(i).x - planets.get(i + 1).x);
+
+            edges.add(new Edge(planets.get(i).num, planets.get(i + 1).num, weight));
+        }
+
+        Collections.sort(planets, Comparator.comparingInt(p -> p.y));
+        for (int i = 0; i < N - 1; i++) {
+            int weight = Math.abs(planets.get(i).y - planets.get(i + 1).y);
+
+            edges.add(new Edge(planets.get(i).num, planets.get(i + 1).num, weight));
+        }
+
+        Collections.sort(planets, Comparator.comparingInt(p -> p.z));
+        for (int i = 0; i < N - 1; i++) {
+            int weight = Math.abs(planets.get(i).z - planets.get(i + 1).z);
+
+            edges.add(new Edge(planets.get(i).num, planets.get(i + 1).num, weight));
+        }
+
+        parent = new int[N];
+        for (int i = 0; i < N; i++) {
+            parent[i] = i;
+        }
+
+        edges.sort((o1, o2) -> Math.toIntExact(o1.weight - o2.weight));
+
+        parent = new int[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            parent[i] = i;
+        }
+        System.out.println(Kruscal());
+    }
+
+    private static long Kruscal() {
+        long totalWeight = 0;
+        for (Edge edge : edges) {
+            int start = edge.start;
+            int end = edge.end;
+            if (findParent(start) != findParent(end)) {
+                union(start, end);
+                totalWeight += edge.weight;
+            }
+        }
+        return totalWeight;
+    }
+
+    private static int findParent(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = findParent(parent[x]);
+    }
+
+    private static void union(int v1, int v2) {
+        v1 = findParent(v1);
+        v2 = findParent(v2);
+        if (v1 != v2) {
+            parent[v2] = v1;
+        }
+    }
+}
+
+/*
+6
+0 0 0
+1 1 1
+-5 -5 -5
+-6 -6 170
+-1 -1 170
+-4 -4 172
+
+답 : 4
+
+ */
