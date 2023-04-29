7e1c411bf6462c88423609218006f336f2623ac7
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-04-29 16:30:24 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.38 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        char[] tmp = my_string.toLowerCase().toCharArray();
+        Arrays.sort(tmp);
+        return new String(tmp);
+    }
+}
