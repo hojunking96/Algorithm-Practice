9725eb55177596c451fdeefb057eecf3f7ff8ed9
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-04-30 21:46:42 +0900

    [unrated] Title: 문자열 뒤집기, Time: 9.61 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuffer sb = new StringBuffer(my_string.substring(s, e + 1));
+        String reverse = sb.reverse().toString();
+        return my_string.substring(0, s) + reverse + my_string.substring(e + 1);
+    }
+}
