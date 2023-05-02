a93b8d77b375b9dadeda9ca6454c70d139121769
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-05-02 23:35:16 +0900

    [level 2] Title: [3차] 압축, Time: 21.96 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String msg) {
+        List<Integer> arr = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        int index;
+
+        for (index = 1; index <= 26; index++) {
+            map.put("" + (char) ('A' + index - 1), index);
+        }
+        for (int i = 0; i < msg.length(); i++) {
+            String key = "" + msg.charAt(i);
+            int num = 0;
+            while (map.containsKey(key)) {
+                i++;
+                num = map.get(key);
+                if (i == msg.length()) {
+                    break;
+                }
+                key += msg.charAt(i);
+            }
+            map.put(key.substring(0, key.length()), index++);
+            arr.add(num);
+            i--;
+            if (i == msg.length() - 1) {
+                break;
+            }
+        }
+        return arr.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
