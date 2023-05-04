833c001a21c5903f1bb2001e2c6047ff6d0e582a
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-05-04 16:44:21 +0900

    [level 2] Title: 땅따먹기, Time: 16.47 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,32 @@
+class Solution {
+    int solution(int[][] land) {
+
+        int n = land.length;
+        int[][] dp = new int[n][4];
+        
+        for(int i = 0; i < 4; i++){
+            dp[0][i] = land[0][i];
+        }
+        
+        /*
+        자, 0번은 나머지 3개중 최대 + 현재
+        
+        */
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < 4; j++){
+                for(int k = 0 ; k < 4; k++){
+                    if(j != k){
+                        dp[i][j] = Math.max(dp[i][j], dp[i - 1][k]);
+                    }
+                }
+                dp[i][j] += land[i][j];
+            }
+        }
+        int max = 0;
+        for(int i = 0; i < 4; i++){
+            max = Math.max(max, dp[n - 1][i]);
+        }
+        
+        return max;
+    }
+}
