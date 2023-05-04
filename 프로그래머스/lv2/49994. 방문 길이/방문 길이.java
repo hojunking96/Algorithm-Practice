aae56cf60c76c9070c78a9777dc3c69df67fe551
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-05-04 16:23:41 +0900

    [level 2] Title: 방문 길이, Time: 0.48 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    
+    //0 : L, 1 : D, 2 : U, 3 : R
+    private static boolean[][][] visited;
+    private static int[] dX = {0, 1, -1, 0};
+    private static int[] dY = {-1, 0, 0, 1};
+    private static Map<Character, Integer> map = new HashMap<>();
+    
+    public int solution(String dirs) {
+        visited = new boolean[11][11][4];
+        map.put('L', 0);
+        map.put('D', 1);
+        map.put('U', 2);
+        map.put('R', 3);
+        
+        char[] arr = dirs.toCharArray();
+        int nowX = 0;
+        int nowY = 0;
+        int totalMoveCnt = 0;
+        int duplicateCnt = 0;
+        for(char e : arr){
+            int direction = map.get(e);
+            int nextX = nowX + dX[direction];
+            int nextY = nowY + dY[direction];
+            if(nextX < -5 || nextX > 5 || nextY < -5 || nextY > 5){
+                continue;
+            }
+            totalMoveCnt++;
+            if(visited[nowX + 5][nowY + 5][direction] || visited[nextX + 5][nextY + 5][3 - direction]){
+                duplicateCnt++;
+            }
+            visited[nowX + 5][nowY + 5][direction]  = true;
+            visited[nextX + 5][nextY +5][3 - direction] = true;
+            nowX = nextX;
+            nowY = nextY;
+        }
+        return totalMoveCnt - duplicateCnt;
+    }
+}
