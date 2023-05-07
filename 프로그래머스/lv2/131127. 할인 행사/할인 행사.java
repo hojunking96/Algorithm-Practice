a63b0654c3e6d9b8b804f62b74afc658521b824a
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-05-07 18:43:48 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    private static Map<String, Integer> map;
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        map = new HashMap<>();
+        int len = discount.length;
+        
+        for(int i = 0; i < len; i++){
+            map.put(discount[i], 0);
+        }
+        for(int i = 0; i < 10; i++){
+            plus(discount[i]);
+        }
+        if(check(want, number)){
+            answer++;
+        }
+        
+        int left = 0;
+        for(int i = 10; i < len; i++){
+            plus(discount[i % len]);
+            minus(discount[left++ % len]);
+            if(check(want, number)){
+                answer++;
+            }
+        }
+        
+        
+        return answer;
+    }
+    
+    private static void plus(String key){
+        int value = map.get(key);
+        map.put(key, value + 1);
+    }
+    
+    private static void minus(String key){
+        int value = map.get(key);
+        map.put(key, value - 1);
+    }
+    
+    private static boolean check(String[] want, int[] number){
+        for(int j = 0 ; j < want.length; j++){
+            if(map.containsKey(want[j]) && map.get(want[j]) == number[j]){
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+}
