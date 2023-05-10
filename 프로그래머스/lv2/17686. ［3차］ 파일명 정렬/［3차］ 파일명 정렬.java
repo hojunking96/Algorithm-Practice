8367f3901f689536ebd4a77f2c24c03b6eca0b2b
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-05-10 11:27:25 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 26.79 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    private static class File {
+        String head;
+        String number;
+        String tail;
+        public File(String head, String number, String tail){
+            this.head = head;
+            this.number = number;
+            this.tail = tail;
+        }
+    }
+    public String[] solution(String[] files) {
+        List<File> list = new ArrayList<>();
+        for(String e : files){
+            int numStartPtr = -1;
+            int numEndPtr = e.length() - 1;
+            for(int i = 0; i < e.length(); i++){
+                if(e.charAt(i) >= '0' && e.charAt(i) <= '9'){
+                    numStartPtr = i;
+                    break;
+                }
+            }
+            for(int i = numStartPtr ; i < e.length(); i++){
+                if(e.charAt(i) < '0' || e.charAt(i) > '9'){
+                    numEndPtr = i - 1;
+                    break;
+                }
+            }
+            String head = e.substring(0, numStartPtr);
+            String number = e.substring(numStartPtr, numEndPtr + 1);
+            String tail = e.substring(numEndPtr + 1);
+            list.add(new File(head, number, tail));
+        }
+        list.sort((o1, o2) -> {
+            String str1 = o1.head.toLowerCase();
+            String str2 = o2.head.toLowerCase();
+            if (str1.compareTo(str2) == 0) {
+                int n1 = Integer.parseInt(o1.number);
+                int n2 = Integer.parseInt(o2.number);
+                return n1 - n2;
+            }
+            return str1.compareTo(str2);
+        });        
+        List<String> arr = new ArrayList<>();
+        for(File e : list){
+            arr.add(e.head + e.number + e.tail);
+        }
+        return arr.toArray(new String[arr.size()]);
+    }
+}
