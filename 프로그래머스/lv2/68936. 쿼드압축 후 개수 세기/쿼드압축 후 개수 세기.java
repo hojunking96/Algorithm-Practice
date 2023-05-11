9af22dbf918f4cf3b48a363727eb43074f224526
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-05-11 09:45:53 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 9.35 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -0,0 +1,40 @@
+class Solution {
+    
+    private static int cnt[];
+    private static boolean[] compacted;
+    
+    public int[] solution(int[][] arr) {
+        cnt = new int[2];
+        compact(arr, 0, 0, arr.length, arr.length);
+        return cnt;
+    }
+    private static void compact(int[][]arr, int startX, int startY, int endX, int endY){
+        int value = arr[startX][startY];
+        boolean isSame = true;
+        for(int i = startX; i < endX; i++){
+            for(int j = startY; j < endY; j++){
+                if(arr[i][j] != value){
+                    isSame = false;
+                    break;
+                }
+            }
+        }
+        if(isSame){
+            cnt[value]++;
+            return;
+        }
+        else{
+            compact(arr, startX, startY, (startX + endX) / 2, (startY + endY) / 2);
+            compact(arr, (startX + endX) / 2, startY, endX, (startY + endY) / 2);
+            compact(arr, startX, (startY + endY) / 2, (startX + endX) / 2, endY);
+            compact(arr, (startX + endX) / 2, (startY + endY) / 2, endX, endY);
+        }
+    }
+}
+
+
+/*
+2^n * 2^n 배열
+
+
+*/
