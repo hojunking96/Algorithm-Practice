0aba7b8e8727a92512214d03bfd0e5de6e589ae8
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-05-11 09:03:20 +0900

    [level 2] Title: 타겟 넘버, Time: 0.37 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,22 @@
 class Solution {
+    
     private static int N;
-    private static int cnt=0;
+    private static int cnt = 0;
+    
     public int solution(int[] numbers, int target) {
         N = numbers.length;
-        DFS(numbers,target,0,0);
+        DFS(numbers, target, 0, 0);
         return cnt;
     }
-    public static void DFS(int[]numbers,int target,int index,int result){
-        if(index==N){
-            if(result==target){
+    
+    public static void DFS(int[] numbers, int target, int index, int result){
+        if(index == N){
+            if(result == target){
                 cnt++;
             }
             return;
         }
-        DFS(numbers,target,index+1,result+numbers[index]);
-        DFS(numbers,target,index+1,result-numbers[index]);
+        DFS(numbers, target,index + 1, result + numbers[index]);
+        DFS(numbers, target,index + 1, result - numbers[index]);
     }
 }
