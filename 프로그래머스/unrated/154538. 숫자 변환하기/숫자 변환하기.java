cf30b25178d8665f52aa546064a5ae75052dccdb
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-05-11 09:21:43 +0900

    [unrated] Title: 숫자 변환하기, Time: 4.20 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    
+    private static int[] cnt;
+    
+    public int solution(int x, int y, int n) {
+        cnt = new int[1_000_001];
+        doCalc(x, y, n);
+        if(cnt[y] == 0 && y != x){
+            return -1;
+        }
+        return cnt[y];
+    }
+    
+    private static void doCalc(int x, int y, int n){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(x);
+        while(!q.isEmpty()){
+            int now = q.poll();
+            if(now == y){
+                return;
+            }
+            int next = now * 3;
+            if(next <= y){                
+                if(cnt[next] == 0){
+                    cnt[next] = cnt[now] + 1;
+                    q.add(next);
+                }
+            }
+            next = now * 2;
+            if(next <= y){                
+                if(cnt[next] == 0){
+                    cnt[next] = cnt[now] + 1;
+                    q.add(next);
+                }
+            } 
+            next = now + n;
+            if(next <= y){                
+                if(cnt[next] == 0){
+                    cnt[next] = cnt[now] + 1;
+                    q.add(next);
+                }
+            }
+        }
+    }
+}
