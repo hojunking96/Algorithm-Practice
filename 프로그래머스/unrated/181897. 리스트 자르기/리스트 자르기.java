206caf1bac443abedb2b91f99b9fcbd7cff1134f
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-05-22 09:24:54 +0900

    [unrated] Title: 리스트 자르기, Time: 2.50 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        List<Integer> arr = new ArrayList<>();
+        int a = slicer[0];
+        int b = slicer[1];
+        int c = slicer[2];
+        
+        if(n == 1){
+            for(int i = 0 ; i <= b; i++){
+                arr.add(num_list[i]);
+            }
+        }
+        else if(n == 2){
+            for(int i = a; i < num_list.length; i++){
+                arr.add(num_list[i]);
+            }
+        }
+        else if(n == 3){
+            for(int i = a; i <= b; i++){
+                arr.add(num_list[i]);
+            }
+        }
+        else{
+            for(int i = a; i <= b; i += c){
+                arr.add(num_list[i]);
+            }
+        }
+        return arr.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
