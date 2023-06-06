09b3043adf99a684dfa1019557de7ee8cc212c59
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-06-07 00:48:34 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.10 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,7 +1,5 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
         String three = Integer.toString(n, 3);
         StringBuffer sb = new StringBuffer(three);
         String reverse = sb.reverse().toString();
