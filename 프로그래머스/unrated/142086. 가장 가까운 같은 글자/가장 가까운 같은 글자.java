b33ec57f6b0df895bd28a4f5dfb4bdd9cc3a4965
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-06-09 09:58:13 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 5.82 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,3 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+         int[] answer = new int[s.length()];
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+                answer[i] = i - map.get(s.charAt(i));
+                map.put(s.charAt(i), i);
+            }
+            else{
+                map.put(s.charAt(i), i);
+                answer[i] = -1;
+            }
+        }
+        return answer;
+    }
+}
+
+/*
 import java.util.stream.IntStream;
 
 class Solution {
@@ -7,3 +28,4 @@ class Solution {
             .toArray();
     }
 }
+*/
