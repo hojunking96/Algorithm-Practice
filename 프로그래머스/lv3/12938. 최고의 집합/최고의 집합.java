e041642b1f4c6fac09497aee3afa01e4e3d6e8ef
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-06-20 01:32:37 +0900

    [level 3] Title: 최고의 집합, Time: 0.01 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int s) {
+        if(n > s){
+            return new int[]{-1};
+        }
+        int[] answer = new int[n];
+        Arrays.fill(answer, s / n);
+        for(int i = 0; i < s % n; i++){
+            answer[n - 1 - i]++;
+        }
+        return answer;
+    }
+}
