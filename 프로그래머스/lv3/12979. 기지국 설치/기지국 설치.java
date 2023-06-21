0983af7a8bc3cbc37a617885c59e55079271d6bc
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-06-21 10:50:14 +0900

    [level 3] Title: 기지국 설치, Time: 2.09 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        List<Integer> arr = new ArrayList<>();
+        int start = 1;
+        int end = 0;
+        for(int station : stations){
+            int point = station - w - 1;
+            end = point;
+            if(end >= start){
+                arr.add(end - start + 1);      
+            }
+            start = station + w + 1;
+        }
+        if(n >= start){
+            arr.add(n - start + 1);        
+        }
+        int len = 2 * w + 1;
+        for(int k : arr){
+            answer += k / len;
+            if(k % len != 0){
+                answer++;
+            }
+        }
+         
+        
+        return answer;
+    }
+}
+/*
+구간 길이를 구해
+
+
+*/
