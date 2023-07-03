310c89410d69d7c11726afcf895f4045767dd256
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-07-03 09:30:35 +0900

    [level 3] Title: 합승 택시 요금, Time: 31.84 ms, Memory: 68.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    private int[][] graph;
+    
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        graph = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j){
+                    graph[i][j] = 0;
+                }
+                else{
+                    graph[i][j] = 1_000_000;
+                } 
+            }
+        }
+        for(int[]fare : fares){
+            graph[fare[0] - 1][fare[1] - 1] = fare[2];
+            graph[fare[1] - 1][fare[0] - 1] = fare[2];
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    graph[j][k] = Math.min(graph[j][k], graph[j][i] + graph[i][k]);
+                }
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < n ; i++){
+                min = Math.min(min, graph[i][a - 1] + graph[i][b - 1] + graph[s - 1][i]);
+        }
+        return min;
+    }
+}
