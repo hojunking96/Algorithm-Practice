e57a684ab987677056ae3b909a5737af423bce6b
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-08-01 03:47:20 +0900

    [unrated] Title: 배열 만들기 6, Time: 14.04 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        
+        int i = 0;
+        while(i < arr.length){
+            int len = stk.size();
+            if(len == 0){
+                stk.add(arr[i]);
+                i++;
+            }
+            else if(stk.get(len - 1) == arr[i]){
+                stk.remove(len - 1);
+                i++;
+            }
+            else{
+                stk.add(arr[i]);
+                i++;
+            }
+        }
+        if(stk.isEmpty()){
+            return new int[]{-1};
+        }
+        
+        return stk.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
+    }
+}
