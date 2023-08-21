6c0cb9c39007da261bb57a90fa3d387c746a382d
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-08-22 00:55:40 +0900

    [level 2] Title: 단체사진 찍기, Time: 3356.24 ms, Memory: 384 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class Solution {
+    
+    private String[] members = {"A", "C", "F", "J", "M", "N", "R", "T"};
+    private int cnt = 0;
+    private String[] conditions;
+    
+    public int solution(int n, String[] data) {
+        conditions = data;
+        
+        String line = "";
+        dd(line);
+        return cnt;
+    }
+    private void dd(String line){
+        if(line.length() == 8){
+            if(check(line)){
+                cnt++;
+            }
+            return;
+        }
+        for(int i = 0; i < 8; i++){
+            if(!line.contains(members[i])){
+                line += members[i];
+                dd(line);
+                line = line.substring(0, line.length() - 1);
+            }
+        }
+    }
+    
+    private boolean check(String line){
+        for(String expression : conditions){
+            String v1 = expression.substring(0, 1);
+            String v2 = expression.substring(2, 3);
+            String func = expression.substring(3, 4);
+            int amount = Integer.parseInt(expression.substring(4, 5)) + 1;
+            
+            int idx1 = line.indexOf(v1);
+            int idx2 = line.indexOf(v2);
+            int diff = Math.abs(idx1 - idx2);
+            if(func.equals("=")){
+                if(diff != amount){
+                    return false;
+                }
+            }
+            else if(func.equals("<")){
+                if(diff >= amount){
+                    return false;
+                }
+            }
+            else{
+                if(diff <= amount){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+/*
+A, C, F, J, M, N, R, T 총 8개
+간격 : 0 ~ 6 (나란히 있는 경우 : 0)
+경우의 수 계산
+
+완탐하면 오래걸리겠지?
+
+
+*/
