c3a3a03919f662bcdd30a0a09e8766876e8fd9a0
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-09-13 01:23:23 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 51.05 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int e : arr){
+            if(stack.isEmpty()){
+                stack.push(e);
+                continue;
+            }
+            if(stack.peek() != e){
+                stack.push(e);
+            }
+        }
+        
+        int[] answer = new int[stack.size()];
+        for(int i = stack.size() - 1; i >= 0; i--){
+            answer[i] = stack.pop();
+        }
+        return answer;
+    }
+}
